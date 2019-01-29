e8c28288e0a58262e01a36b636f09dd980da49f5
Bruce Goldstein <bruce.goldstein@janushenderson.com>
2019-01-29 15:14:43 -0700

    Added feature examples

---
+++ BlazorApplication/CounterClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+
+namespace BlazorApplication
+{
+    public class CounterClass : BlazorComponent
+    {
+        public int CurrentCount { get; set; }
+
+        [Parameter]
+        protected string SubTitle { get; set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount += 5;
+        }
+    }
+}
